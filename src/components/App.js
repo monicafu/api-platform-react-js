9bb1c229403af291e3341e92d25ac71c6d3a6a64
monicafu <31578677+monicafu@users.noreply.github.com>
2019-08-27 16:09:24 -0700

    add some components page

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route,Switch} from 'react-router';
+import LoginForm from './LoginForm';
+import BlogPostListContainer from './BlogPostListContainer';
+
+class App extends React.Component {
+    render() {
+        return (
+        <div>
+            Hello!
+            <Switch>
+                <Route path ="/login" component = {LoginForm}/>
+                <Route path ="/" component = {BlogPostListContainer}/>
+            </Switch>
+        </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/BlogPostListContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BlogPostList from './BlogPostList';
+
+class BlogPostListContainer  extends React.Component {
+    constructor(props) {
+        super(props);
+        this.posts = [
+            {
+                id : 1,
+                title:'Hello'
+            },
+            {
+                id : 2,
+                title:'Another post'
+            }
+        ];
+    }
+    render() {
+        return (<BlogPostList posts = {this.posts}/>)
+    }
+}
+
+export default BlogPostListContainer;
