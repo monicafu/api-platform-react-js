f7704bf3765530476e59f37086b7f3af10c19795
monicafu <31578677+monicafu@users.noreply.github.com>
2019-08-28 16:57:39 -0700

    Added route to individual blogpost

--- src/components/BlogPostListContainer.js
+++ src/components/BlogPostListContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BlogPostList from './BlogPostList';
-import { blogPostListFetch,blogPostAdd } from '../actions/actions';
+import { blogPostListFetch } from '../actions/actions';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
@@ -8,20 +8,19 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-    blogPostAdd,
     blogPostListFetch
 };
 
 class BlogPostListContainer  extends React.Component {
     componentDidMount() {
-        setTimeout(this.props.blogPostAdd,1000);
-        setTimeout(this.props.blogPostAdd,3000);
-        setTimeout(this.props.blogPostAdd,5000);
+        // setTimeout(this.props.blogPostAdd,1000);
+        // setTimeout(this.props.blogPostAdd,3000);
+        // setTimeout(this.props.blogPostAdd,5000);
         this.props.blogPostListFetch();
     }
     render() {
         const {posts,isFetching} = this.props;
-        
+
         return (<BlogPostList posts = {posts} isFetching={isFetching}/>)
     }
 }
