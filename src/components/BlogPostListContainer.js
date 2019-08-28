ff4b1824fabf01e4e46328882e51972c6b228372
monicafu <31578677+monicafu@users.noreply.github.com>
2019-08-28 11:19:08 -0700

    Add thunk redux middleware to get the postlist from server Api

--- src/components/BlogPostListContainer.js
+++ src/components/BlogPostListContainer.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import BlogPostList from './BlogPostList';
-import { blogPostList,blogPostAdd } from '../actions/actions';
+import { blogPostListFetch,blogPostAdd } from '../actions/actions';
 import { connect } from 'react-redux';
-import { requests } from '../agent';
 
 const mapStateToProps = state => ({
     ...state.blogPostList
 });
 
 const mapDispatchToProps = {
-    blogPostList,
-    blogPostAdd
+    blogPostAdd,
+    blogPostListFetch
 };
 
 class BlogPostListContainer  extends React.Component {
     componentDidMount() {
-        requests.get('/blog_posts').then(response => console.log(response));
         setTimeout(this.props.blogPostAdd,1000);
         setTimeout(this.props.blogPostAdd,3000);
         setTimeout(this.props.blogPostAdd,5000);
-        this.props.blogPostList();
+        this.props.blogPostListFetch();
     }
     render() {
-        console.log(this.props.posts);
+        console.log(this.props);
         return (<BlogPostList posts = {this.props.posts}/>)
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import createHistory from 'history/createBrowserHistory';
 import {Provider} from 'react-redux';
 import {ConnectedRouter} from 'react-router-redux';
 import {Route} from 'react-router';
 import App from './components/App';
 import reducer from './reducer';
+import thunkMiddleware from 'redux-thunk';
 
-
-const store = createStore(reducer);
+const store = createStore(reducer,applyMiddleware(thunkMiddleware));
 
 const history = createHistory();
 
